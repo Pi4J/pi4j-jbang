8b8312428441e8e6b998d208a45ea5888dbf622d
Frank Delporte <fdelporte@azul.com>
2023-07-07 14:39:35 +0200

    WIP on new examples

--- Pi4JPixelblazeOutputExpander.java
+++ Pi4JPixelblazeOutputExpander.java
@@ -7,17 +7,15 @@
 //DEPS com.pi4j:pi4j-plugin-pigpio:2.3.0
 
 import com.pi4j.Pi4J;
-import com.pi4j.io.gpio.digital.DigitalInput;
-import com.pi4j.io.gpio.digital.DigitalOutput;
-import com.pi4j.io.gpio.digital.DigitalState;
-import com.pi4j.io.gpio.digital.PullResistance;
+import com.pi4j.io.serial.FlowControl;
+import com.pi4j.io.serial.Parity;
+import com.pi4j.io.serial.Serial;
+import com.pi4j.io.serial.StopBits;
 import com.pi4j.util.Console;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.zip.CRC32;
-
-import com.fazecast.jSerialComm.SerialPort;
 
 /**
  * Thanks to Jeff Vyduna for his Java driver for the Output Expander that has been used in this example.
@@ -32,31 +30,52 @@ public class Pi4JPixelblazeOutputExpander {
     private static final byte CH_DRAW_ALL = 2;
     private static final byte CH_APA102_DATA = 3;
     private static final byte CH_APA102_CLOCK = 4;
+    private static Console console;
+    private static ExpanderDataWriteAdapter adapter;
 
     public static void main(String[] args) throws Exception {
-
-        final var console = new Console();
-
+        console = new Console();
         var pi4j = Pi4J.newAutoContext();
+        var serialConfig = Serial.newConfigBuilder(pi4j)
+                .baud(2_000_000)
+                .dataBits_8()
+                .parity(Parity.NONE)
+                .stopBits(StopBits._1)
+                .flowControl(FlowControl.NONE)
+                .id("my-serial")
+                .device("/dev/ttyS0")
+                .provider("pigpio-serial")
+                .build();
+        /*
+        Config used by Jeff Vyduna
+        port.setBaudRate(2_000_000);
+        port.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING, 0, 0);
+        port.openPort(0, 8192, 8192);
+        */
+        var serial = pi4j.create(serialConfig);
+        serial.open();
 
+        adapter = new ExpanderDataWriteAdapter(serial, true);
+        byte[] pixelData = new byte[]{(byte) 0xff, (byte) 0x00, (byte) 0x00};;
 
+        sendWs2812(0, 3, 0, 0, 0, 0, pixelData);
 
         pi4j.shutdown();
     }
 
-    ExpanderDataWriteAdapter writeAdapter;
-
-    public ExpanderDriver(ExpanderDataWriteAdapter adapter) {
-        this.writeAdapter = adapter;
-    }
-
-    public synchronized void sendWs2812(int channel, int bytesPerPixel, int rIndex, int gIndex, int bIndex, int wIndex, byte[] pixelData) {
-        checkArgument(bytesPerPixel == 3 || bytesPerPixel == 4);
-        checkArgument(rIndex <= 3);
-        checkArgument(gIndex <= 3);
-        checkArgument(bIndex <= 3);
-        checkArgument(wIndex <= 3);
-        checkNotNull(pixelData);
+    private static void sendWs2812(int channel, int bytesPerPixel, int rIndex, int gIndex, int bIndex, int wIndex, byte[] pixelData) {
+        if (bytesPerPixel != 3 && bytesPerPixel != 4) {
+            console.println("bytesPerPixel not within expected range");
+            return;
+        }
+        if (rIndex > 3 || gIndex > 3 || bIndex > 3 || wIndex > 3) {
+            console.println("one or more indexes not within expected range");
+            return;
+        }
+        if (pixelData == null) {
+            console.println("pixelData can not be null");
+            return;
+        }
 
         int pixels = pixelData.length / bytesPerPixel;
         CRC32 crc = new CRC32();
@@ -68,31 +87,31 @@ public class Pi4JPixelblazeOutputExpander {
         byte[] bytes = buffer.array();
 
         crc.update(bytes);
-        writeAdapter.write(bytes);
+        adapter.write(bytes);
 
         crc.update(pixelData);
-        writeAdapter.write(pixelData);
+        adapter.write(pixelData);
 
         writeCrc(crc);
     }
 
-    private void writeCrc(CRC32 crc) {
+    private static void writeCrc(CRC32 crc) {
         byte[] crcBytes = new byte[4];
         packInt(crcBytes, 0, (int) crc.getValue());
-        writeAdapter.write(crcBytes);
+        adapter.write(crcBytes);
     }
 
-    public synchronized void sendDrawAll() {
+    public static void sendDrawAll() {
         CRC32 crc = new CRC32();
         crc.reset();
         ByteBuffer buffer = initHeaderBuffer(6, (byte) 0xff, CH_DRAW_ALL);
         byte[] bytes = buffer.array();
         crc.update(bytes);
-        writeAdapter.write(bytes);
+        adapter.write(bytes);
         writeCrc(crc);
     }
 
-    void packInt(byte[] outgoing, int index, int val) {
+    private static void packInt(byte[] outgoing, int index, int val) {
         outgoing[index++] = (byte) (val & 0xFF); val = val >> 8;
         outgoing[index++] = (byte) (val & 0xFF); val = val >> 8;
         outgoing[index++] = (byte) (val & 0xFF); val = val >> 8;
@@ -112,36 +131,23 @@ public class Pi4JPixelblazeOutputExpander {
         return buffer;
     }
 
-    class ExpanderDataWriteAdapter {
+    static class ExpanderDataWriteAdapter {
 
-        private SerialPort port = null;
-        private final String portPath;
+        private Serial serial;
         private boolean debug = false;
 
-        public SerialPortAdapter(String portPath, boolean debug) {
+        public ExpanderDataWriteAdapter(Serial serial, boolean debug) {
+            this.serial = serial;
             this.debug = debug;
-            this.portPath = portPath;
-        }
-
-        private void openPort() {
-            if (port != null)
-                port.closePort();
-            port = null; //set to null in case getCommPort throws, port will remain null.
-            port = SerialPort.getCommPort(this.portPath);
-            port.setBaudRate(2000000);
-            port.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING, 0, 0);
-            port.openPort(0, 8192, 8192);
-            System.out.println("Opening " + portPath);
         }
 
         public void write(byte[] data) {
-            int lastErrorCode = port != null ? port.getLastErrorCode() : 0;
-            boolean isOpen = port != null && port.isOpen();
-            if (port == null || !isOpen || lastErrorCode != 0) {
-                System.out.println("port was open:" + isOpen + " last error:" + lastErrorCode);
-                openPort();
+            boolean isOpen = serial != null && serial.isOpen();
+            if (serial == null || !isOpen) {
+                console.println("Port open:" + isOpen);
+                return;
             }
-            port.writeBytes(data, data.length);
+            // TODO serial.writeBytes(data, data.length);
             if (debug) {
                 for (int i = 0; i < data.length; i++) {
                     System.out.printf("%02x ", data[i]);
