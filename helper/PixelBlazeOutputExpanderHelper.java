316bd74f5ec142769eea27e36626a039ca0574a9
Frank Delporte <fdelporte@azul.com>
2023-09-27 15:50:27 +0200

    Make sure all pending PBOE commands are sent before closing the port

--- helper/PixelBlazeOutputExpanderHelper.java
+++ helper/PixelBlazeOutputExpanderHelper.java
@@ -2,6 +2,7 @@ package helper;
 
 import com.fazecast.jSerialComm.SerialPort;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.zip.CRC32;
@@ -145,6 +146,12 @@ public class PixelBlazeOutputExpanderHelper {
         private void closePort() {
             if (port != null) {
                 System.out.println("Closing " + portPath);
+                try {
+                    // Make sure all pending commands are sent before closing the port
+                    port.getOutputStream().flush();
+                } catch (IOException e) {
+                    System.err.println("Error while flushing the data: " + e.getMessage());
+                }
                 port.closePort();
             }
         }
